7c2b420ba9a457238c56475aa48d1aa3b0c8d745
Emil Hansen <emil901h@gmail.com>
2025-02-28 13:02:46 +0100

    goal conditions

--- python_code/compiler_filer.py
+++ python_code/compiler_filer.py
@@ -4,17 +4,17 @@ import generating_boards as gb
 
 
 def board_p(b):
+    out = ""
     for i in range(2):
         for j in range(3):
             if(b[i,j]==0):
-                print("open("+str(i)+","+str(j)+") ")
+                out += ("open("+str(i)+","+str(j)+") ")
             if(b[i,j]==1):
-                print("black("+str(i)+","+str(j)+") ")
+                out += ("black("+str(i)+","+str(j)+") ")
             if(b[i,j]==2):
-                print("white("+str(i)+","+str(j)+") ")
+                out += ("white("+str(i)+","+str(j)+") ")
+    return out
 
 
 for i in range(len(gb.final_boards)):
-    print("not(")
-    board_p(gb.final_boards[i])
-    print(") ")
+    print("not(" + (board_p(gb.final_boards[i])) + ")")
