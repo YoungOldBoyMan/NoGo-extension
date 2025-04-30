a6d1968b0b27e5c8078e9276143ca6a43fed30c5
Emil Hansen <emil901h@gmail.com>
2025-04-30 09:57:02 +0200

    added space and rename

--- python_code/compiler_filer.py
+++ python_code/compiler_filer.py
@@ -1,39 +1,37 @@
 import generating_boards as gb
 
 
-
-
 def board_p(b):
     out = ""
     for i in range(3):
         for j in range(3):
-            if(b[i,j]==0):
+            if (b[i, j] == 0):
                 out += ("open("+str(i)+","+str(j)+") ")
-            if(b[i,j]==1):
+            if (b[i, j] == 1):
                 out += ("black("+str(i)+","+str(j)+") ")
-            if(b[i,j]==2):
+            if (b[i, j] == 2):
                 out += ("white("+str(i)+","+str(j)+") ")
     return out
 
 
-#for i in range(3):
+# for i in range(3):
 #    print("not(" + (board_p(gb.final_boards[i])) + ")")
-
 
 
 def as_Sets(b):
     out = set()
     for i in range(3):
         for j in range(3):
-            if(b[i,j]==0):
+            if (b[i, j] == 0):
                 continue
-            if(b[i,j]==1):
+            if (b[i, j] == 1):
                 s = "1"+str(i)+str(j)
                 out.add(s)
-            if(b[i,j]==2):
+            if (b[i, j] == 2):
                 s = "2"+str(i)+str(j)
                 out.add(s)
     return out
+
 
 list1 = []
 for i in range(len(gb.final_boards)):
@@ -45,8 +43,8 @@ for i in range(len(list1)):
     d = list1[i]
     for j in range(len(list1)-1-i):
         sec = list1[j+1+i]
-        if(d.issubset(sec)):
-            if(list1[j+1+i] in nyliste):
+        if (d.issubset(sec)):
+            if (list1[j+1+i] in nyliste):
                 nyliste.remove(list1[j+1+i])
 
 
@@ -54,18 +52,18 @@ print()
 print(nyliste)
 
 
-
 def as_string(b):
     out = ""
     for i in b:
-        if(i[0]=="0"):
+        if (i[0] == "0"):
             out += "open("
-        if(i[0]=="1"):
+        if (i[0] == "1"):
             out += "black("
-        if(i[0]=="2"):
+        if (i[0] == "2"):
             out += "white("
-        out += i[1]+","+i[2]+")"
+        out += i[1]+","+i[2]+") "
     return out
+
 
 endelig = []
 for i in range(len(nyliste)):
@@ -74,11 +72,3 @@ for i in range(len(nyliste)):
     print(j)
 
 print(len(endelig))
-
-
-
-
-
-
-
-
