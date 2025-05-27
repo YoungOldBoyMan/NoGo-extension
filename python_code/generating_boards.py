2131d922e769df8d14b9761ee33c9f2c33c34998
Emil Hansen <emil901h@gmail.com>
2025-05-27 17:26:33 +0200

    done

--- python_code/generating_boards.py
+++ python_code/generating_boards.py
@@ -39,30 +39,31 @@ elif num_1 == num_2 + 1:
 full_code = out_str + "\n" + "    " * (BOARD_X*BOARD_Y) + board_assignment
 full_code += "    " * (BOARD_Y * BOARD_X) + board_logic.replace("\n", "\n" + "    " * (BOARD_X * BOARD_Y))
 
+print(full_code)
 
-for i0 in range(3):
-    for i1 in range(3):
-        for i2 in range(3):
-            for i3 in range(3):
-                for i4 in range(3):
-                    for i5 in range(3):
-                        for i6 in range(3):
-                            for i7 in range(3):
-                                for i8 in range(3):
-                                    for i9 in range(3):
-                                        for i10 in range(3):
-                                            for i11 in range(3):
-                                                for i12 in range(3):
-                                                    for i13 in range(3):
-                                                        for i14 in range(3):
-                                                            for i15 in range(3):
-                                                                pos_boards = np.array([[i0, i1, i2, i3], [i4, i5, i6, i7], [i8, i9, i10, i11], [i12, i13, i14, i15]])
-                                                                num_1 = np.count_nonzero(pos_boards == 1)
-                                                                num_2 = np.count_nonzero(pos_boards == 2)
-                                                                if num_1 == num_2:
-                                                                    black_win_boards.append(pos_boards)
-                                                                elif num_1 == num_2 + 1:
-                                                                    white_win_boards.append(pos_boards)
+# for i0 in range(3):
+#     for i1 in range(3):
+#         for i2 in range(3):
+#             for i3 in range(3):
+#                 for i4 in range(3):
+#                     for i5 in range(3):
+#                         for i6 in range(3):
+#                             for i7 in range(3):
+#                                 for i8 in range(3):
+#                                     for i9 in range(3):
+#                                         for i10 in range(3):
+#                                             for i11 in range(3):
+#                                                 for i12 in range(3):
+#                                                     for i13 in range(3):
+#                                                         for i14 in range(3):
+#                                                             for i15 in range(3):
+#                                                                 pos_boards = np.array([[i0, i1, i2, i3], [i4, i5, i6, i7], [i8, i9, i10, i11], [i12, i13, i14, i15]])
+#                                                                 num_1 = np.count_nonzero(pos_boards == 1)
+#                                                                 num_2 = np.count_nonzero(pos_boards == 2)
+#                                                                 if num_1 == num_2:
+#                                                                     black_win_boards.append(pos_boards)
+#                                                                 elif num_1 == num_2 + 1:
+#                                                                     white_win_boards.append(pos_boards)
 
 def bfs_board(board):
 
